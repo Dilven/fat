5148adeab79946ddb2f51142f4de3157d85520af
Dilven <mateusiakkrystian@gmail.com>
2020-12-31 13:50:02 +0100

    Add key to enviroment variables + add basic headers to constants

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,13 +1,16 @@
 import * as z from 'zod';
 import { Recipe, RecipeFullInfo } from '../schemas/recipe';
 
+const BASE_URL = "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com";
+
+const headers = {
+  "x-rapidapi-key": process.env.REACT_API_KEY || '',
+  "x-rapidapi-host": BASE_URL,
+  "useQueryString": "true"
+}
 export const searchRecipes = async (query: string) => {
   const data: unknown = (await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${query}&number=10`, {
-    headers: {
-      "x-rapidapi-key": "cee42c4284msh6234a9b5d070531p13529djsn38776083e9a4",
-      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      "useQueryString": "true"
-    },
+    headers,
   })).json();
  
   return z.object({results: z.array(Recipe)}).parse(data);
@@ -15,11 +18,7 @@ export const searchRecipes = async (query: string) => {
 
 export const getRecipe = async (id: string) => {
   const data: unknown = (await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`, {
-    headers: {
-      "x-rapidapi-key": "cee42c4284msh6234a9b5d070531p13529djsn38776083e9a4",
-      "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      "useQueryString": "true"
-    },
+    headers,
   })).json();
   
   return RecipeFullInfo.parse(data)
