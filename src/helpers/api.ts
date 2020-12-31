712b72ccdbe5af48d1c7f04252085402ce29df32
Dilven <mateusiakkrystian@gmail.com>
2020-12-31 13:46:17 +0100

    Arrow func instead of named func

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,7 @@
 import * as z from 'zod';
 import { Recipe, RecipeFullInfo } from '../schemas/recipe';
 
-export async function searchRecipes(query: string) {
+export const searchRecipes = async (query: string) => {
   const data: unknown = (await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/search?query=${query}&number=10`, {
     headers: {
       "x-rapidapi-key": "cee42c4284msh6234a9b5d070531p13529djsn38776083e9a4",
@@ -13,7 +13,7 @@ export async function searchRecipes(query: string) {
   return z.object({results: z.array(Recipe)}).parse(data);
 } 
 
-export async function getRecipe(id: string) {
+export const getRecipe = async (id: string) => {
   const data: unknown = (await fetch(`https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/${id}/information`, {
     headers: {
       "x-rapidapi-key": "cee42c4284msh6234a9b5d070531p13529djsn38776083e9a4",
